d5cd45202d921c60e540849f366304063060a02f
Luis Tomas Bolivar <ltomasbo@redhat.com>
2025-09-15 11:53:56 +0200

    First version of streaming_query for Responses API (v2)

--- src/app/routers.py
+++ src/app/routers.py
@@ -11,6 +11,7 @@ from app.endpoints import (
     config,
     feedback,
     streaming_query,
+    streaming_query_v2,
     authorized,
     conversations,
     conversations_v2,
@@ -40,6 +41,7 @@ def include_routers(app: FastAPI) -> None:
 
     # V2 endpoints - Response API support
     app.include_router(query_v2.router, prefix="/v2")
+    app.include_router(streaming_query_v2.router, prefix="/v2")
 
     # road-core does not version these endpoints
     app.include_router(health.router)
